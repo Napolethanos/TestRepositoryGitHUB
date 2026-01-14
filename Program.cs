2da1b1b24f76632d5b7ff1313eb5cbcc78eac757
Natan Sandri <sandrinatan5@gmail.com>
2026-01-14 20:53:07 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestRepositoryGitHUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
